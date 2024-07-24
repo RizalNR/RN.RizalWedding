adfc9a270e4c3e47c55928e59f18aafcc18e79f1
RizalNR <myrizalnr@gmail.com>
2024-07-24 14:54:20 +0700

    full

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -13,39 +13,39 @@ function Swipper() {
       showStatus={false}
       >
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <img src="public/prew1.jpg" />
             {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            <img src="public/billa.jpg" />
             {/* <p className="legend">Legend 2</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            <img src="public/rama.jpg" />
             {/* <p className="legend">Legend 3</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <img src="public/bgchina1.jpg" />
             {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            <img src="public/bgchina2.jpg" />
             {/* <p className="legend">Legend 2</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            <img src="public/bgchina3.jpg" />
             {/* <p className="legend">Legend 3</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <img src="public/bgchina4.jpg" />
             {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            <img src="public/bgchina5.jpeg" />
             {/* <p className="legend">Legend 2</p> */}
         </div>
         <div>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            <img src="public/bgduduk.jpg" />
             {/* <p className="legend">Legend 3</p> */}
         </div>
       </Carousel>
--- src/pages/Reward.jsx
+++ src/pages/Reward.jsx
@@ -20,7 +20,7 @@ const Reward = () => {
     }
   return (
     <div className='w-screen flex flex-col items-center resepsi_3'>
-    <div class="container_top3"><h1>Amplop</h1></div>
+    <div class="container_top3"><h1>Amplop Digital</h1></div>
     <p className='text-center mt-10'>
       Doa Restu Anda merupakan <br />
       karunia yang sangat berarti bagi kami. <br />
@@ -32,31 +32,48 @@ const Reward = () => {
     <button className='mt-10 py-1 px-5 text-white rounded-md bg-blue-500 mb-10' onClick={handleBtn} >Kirim Hadiah</button>
 
     <div className={` font-bold mb-10 mt-5 grid gap-8 'costum-image ${btn ? "hidden opacity-0 transform -translate-y-10" : "block opacity-100 transform translate-y-0"} transition-all duration-1000 ease-in-out` }>
+      {/* ATM 1 */}
       <div className='w-[296px] bg-img h-[154px] bg-slate-200 rounded-md relative'>
         {/* Content card pertama */}
-        <img src="public/BCA_logo_Bank_Central_Asia-1.png" alt="" className='w-20 h-7 absolute right-3 top-3 ' />
+        <img src="public/BCA_logo_Bank_Central_Asia-1.png" alt="" className='w-15 h-5 absolute right-3 top-3 ' />
         <div className='mt-8 ml-2'>
-          <img src="public/chip-atm-1.png" alt="" className='w-8 h-8' />
-          <p id='noRek' className='text-[15px] word-spacing-height'>
-          1831783261
+          <img src="public/chip-atm-1.png" alt="" className='w-8 h-8 absolute left-5 mt-5' />
+          <p id='noRek' className='text-[15px]  py-1 px-2 text-black absolute bottom-10'>
+          6975444752
         </p>
-        <h1>
-        Afni Fauzi
+        <h1 className='py-1 px-2 text-white absolute bottom-5 text-black'>
+        Ayudyah Ainun Nabillah
         </h1>
         </div>
-        <button onClick={copyNoRek} className={`py-1 px-5 text-white rounded-md bg-blue-500 absolute bottom-5 right-3`}>Copy</button>
+        <button onClick={copyNoRek} className={`py-1 px-5 text-white rounded-md bg-blue-400 absolute bottom-5 right-3`}>Copy</button>
       </div>
 
-      <div className='w-[296px] bg-img h-[154px] bg-slate-200 rounded-md text-center'>
+      {/* ATM 2 */}
+      <div className='w-[296px] bg-img h-[154px] bg-slate-200 rounded-md relative'>
+        {/* Content card pertama */}
+        <img src="public/BCA_logo_Bank_Central_Asia-1.png" alt="" className='w-15 h-5 absolute right-3 top-3 ' />
+        <div className='mt-8 ml-2'>
+          <img src="public/chip-atm-1.png" alt="" className='w-8 h-8 absolute left-5 mt-5' />
+          <p id='noRek' className='text-[15px] py-1 px-2 text-black absolute bottom-10'>
+          6975444787
+        </p>
+        <h1 className='py-1 px-2 text-white absolute bottom-5 text-black'>
+        Rizal Nur Ramadhan
+        </h1>
+        </div>
+        <button onClick={copyNoRek} className={`py-1 px-5 text-white rounded-md bg-blue-400 absolute bottom-5 right-3`}>Copy</button>
+      </div>
+
+      <div className='w-[296px] bg-img h-[200px] bg-slate-200 rounded-md text-center'>
         {/* Content card kedua */}
         <div className='mt-2'>
-        <h1 className='text-[20px] ' >🎁</h1>
-        <h1 className='text-[13px] mt-2 mb-2'>
-          kIRIM hADIAH
+        <h1 className='text-[22px] '>🎁🎁🎁</h1>
+        <h1 className='text-[20px] mt-2 mb-2 '>
+          KIRIM HADIAH
         </h1>
-          <h2 className='text-[12px]'>Nama Penerima : Afni Fauzi </h2>
-          <h2 className='text-[13px]'>No. HP : 083816286859 (wa)</h2>
-          <h2 className='text-[13px]'>Alamat : Kp.Babakan Bandung RT 1 RW 10 Ds.  <br />
+          <h2 className='text-[15px]'>Nama Penerima : Billah / Rizal </h2>
+          <h2 className='text-[15px]'>No.HP : 081230211096 </h2>
+          <h2 className='text-[15px]'>Alamat : Kp.Babakan Bandung RT 1 RW 10 Ds.  <br />
           Hegarmanah Kec. Karangtengah Kab. Cianjur</h2>
         </div>
       </div>
