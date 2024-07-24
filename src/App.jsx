181b3076b515750ded70c2dd0ec9ff3f8a562aff
RizalNR <myrizalnr@gmail.com>
2024-07-24 16:52:07 +0700

    perbaiki routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Invitation from "./pages/Invitation";
 
 
 const App = () => {
   return (
-    <div className="body">
 
 
-    <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/invitation" element={<Invitation />} />
       </Routes>
-    </BrowserRouter>
 
-    </div>
+      
+  
   );
 };
 
